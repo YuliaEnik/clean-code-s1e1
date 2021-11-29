6748a6b39c4e377233d051bc9d9217acda0765de
Yuliya Aleinik <aleinikya@mail.ru>
2021-11-29 15:34:19 +0300

    refactor: change class name  rule 3.3

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("in-complete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("todo-block");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed-block");//completed-tasks
 
 
 //New task list item
