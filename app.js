b1f3773174d084d9d6b7cd72dd33949a8313874b
Yuliya Aleinik <aleinikya@mail.ru>
2021-11-30 01:30:09 +0300

    refactor: class name BEM according rules 2.1 in html-and-css-extended.md, 3.1,3.2,3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new-task__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("todo-block");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-block");//completed-tasks
